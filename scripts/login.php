f5c02c6c9c31a1c19af74f85b5f19713659837af
Celes <celestemarmar2@gmail.com>
2023-11-12 20:30:48 -0300

    v0.5.3.14 login fixed to accept hash

--- scripts/login.php
+++ scripts/login.php
@@ -12,29 +12,46 @@
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 		// Use a prepared statement to prevent SQL injection
-        $query = $connection->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
-        $query->bind_param("ss", $username, password_hash($password, PASSWORD_BCRYPT));
+        $query = $connection->prepare("SELECT password FROM users WHERE username = ?");
+        $query->bind_param("s", $username);
         $query->execute();
 
         $result = $query->get_result();
         if ($result->num_rows > 0) {
-                $row = $result->fetch_assoc();
-                $_SESSION["name"] = $row['name'];
-                $_SESSION["surname"] = $row['surname'];
-                $_SESSION["username"] = $row['username'];
-                $_SESSION["password"] = $row['password'];
-                $_SESSION["email"] = $row['email'];
-                $_SESSION["birthdate"] = $row['birthdate'];
-                $_SESSION["sex"] = $row['sex'];
-                $_SESSION["interests"] = $row['interests'];
-                header("Location: ../index.php");
+            $row = $result->fetch_assoc();
+            if (password_verify($password, $row['password'])) {
+                $query = $connection->prepare("SELECT * FROM users WHERE username = ?");
+                $query->bind_param("s", $username);
+                $query->execute();
+
+                $result = $query->get_result();
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $_SESSION["name"] = $row['name'];
+                    $_SESSION["surname"] = $row['surname'];
+                    $_SESSION["username"] = $row['username'];
+                    $_SESSION["email"] = $row['email'];
+                    $_SESSION["birthdate"] = $row['birthdate'];
+                    $_SESSION["sex"] = $row['sex'];
+                    $_SESSION["interests"] = $row['interests'];
+                    header("Location: ../index.php");
+                    exit();
+                }
+                else {
+                    header("Location: ../signin.php");
+                    exit();
+                }
+            }
+            else {
+                header("Location: ../signin.php");
                 exit();
-		}
-		else {
-       			header("Location: ../signin.php");
-       			exit();
-		}
-	}
+            }
+        }
+        else {
+            header("Location: ../signin.php");
+            exit();
+        }
+    }
 	else {
 		header("Location: ../index.php/#");
 		exit();
